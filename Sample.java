2213a52ee558253179044f998fc695f205891f7e
git-nadeem <77234299+git-nadeem@users.noreply.github.com>
2021-08-23 13:58:42 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ class Simple{
 public static void main(String args[]) throws InterruptedException{
 Thread.sleep(10000);
 System.out.println("Hello Java Updated 2nd time");
+System.out.println("Hello Java added to branch1);
 }
 }
