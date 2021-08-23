db5356aa2d22b425c89f77c3e36dde98a8971760
git-nadeem <77234299+git-nadeem@users.noreply.github.com>
2021-08-23 14:14:25 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,5 +3,6 @@ public static void main(String args[]) throws InterruptedException{
 Thread.sleep(10000);
 System.out.println("Hello Java Updated 2nd time");
 System.out.println("Hello Java added to branch1");
+  System.out.println("Hello Java added to branch1 to check sleep");
 }
 }
