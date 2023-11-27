508a0ad8d550564ba796ceb6157d73146de30c87
Nadeem <mohammad.d.nadeem@accenture.com>
2023-11-27 19:44:18 +0530

    First test on GITHUB

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 class Simple{
 public static void main(String args[]) throws InterruptedException{
 Thread.sleep(10000);
-System.out.println("Hello Java Updated 2nd time");
+System.out.println("This is my first TEST on GITHUB");
 }
 }
